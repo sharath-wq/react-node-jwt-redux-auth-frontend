c81e6b3186718c38203bd77e8fc8614ec1073534
sharath <admin@Macbook-Air.local>
2023-12-22 18:36:49 +0530

    prevent showing login page while logged in

--- src/router.jsx
+++ src/router.jsx
@@ -1,19 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Login, SignUp } from "./pages";
 import Home from "./pages/Home";
+import ProtectedRoute from "./pages/Protected";
+import IsLoggedIn from "./pages/IsLoggedIn";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />,
+        element: (
+            <ProtectedRoute>
+                <Home />
+            </ProtectedRoute>
+        ),
     },
     {
         path: "/login",
-        element: <Login />,
+        element: (
+            <IsLoggedIn>
+                <Login />
+            </IsLoggedIn>
+        ),
     },
     {
         path: "/signup",
-        element: <SignUp />,
+        element: (
+            <IsLoggedIn>
+                <SignUp />
+            </IsLoggedIn>
+        ),
     },
 ]);
 
